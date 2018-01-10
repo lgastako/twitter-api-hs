466e7a1762211ea3683aa27322d33cb36bcafb6a
Juan Pablo Royo <juanpablo.royo@gmail.com>
2018-01-10 15:29:15 +0100

    Initial commit

---
+++ src/Twitter/Adapter.hs
@@ -0,0 +1,63 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+module Twitter.Adapter (
+userTimeline
+) where
+
+import           GHC.Generics
+import           Control.Applicative
+import           Data.Aeson
+import qualified Data.ByteString.Char8      as S8
+import qualified Data.ByteString.Lazy.Char8 as L8
+import           Data.ByteString.Conversion
+import           Data.Maybe (fromMaybe)
+import qualified Data.Text.Encoding         as E
+import           Data.Text (Text)
+import qualified Data.Yaml                  as Yaml
+import           Network.HTTP.Simple
+import           Network.HTTP.Client
+import           Twitter.Config
+
+data Token = Token { tokenType :: Text, accessToken :: Text } deriving (Generic, Show)
+
+instance FromJSON Token where
+    parseJSON (Object v) = Token <$> v .: "token_type" <*> v .: "access_token"
+    parseJSON _          = empty
+
+instance ToJSON Token where
+    toJSON (Token tokenType accessToken) = object ["token_type" .= tokenType, "access_token" .= accessToken]
+
+consumerEnc :: IO S8.ByteString
+consumerEnc = do
+  c <- config
+  return (twitterEncKey c)
+
+bearer :: IO Token
+bearer = do
+    token <- consumerEnc
+    request' <- parseRequest "https://api.twitter.com"
+    let request
+            = setRequestMethod "POST"
+            $ setRequestHeader "Authorization" [S8.concat ["Basic ", token]]
+            $ setRequestHeader "Content-Type" ["application/x-www-form-urlencoded;charset=UTF-8"]
+            $ setRequestPath "/oauth2/token"
+            $ setRequestBodyLBS "grant_type=client_credentials"
+            $ setRequestSecure True
+            $ setRequestPort 443 request'
+    response <- httpJSON request
+    return (getResponseBody response :: Token)
+
+
+userTimeline :: S8.ByteString -> Maybe Int -> IO ()
+userTimeline name limit = do
+    token <- bearer
+    request' <- parseRequest "https://api.twitter.com"
+    let request
+            = setRequestMethod "GET"
+            $ setRequestHeader "Authorization" [S8.concat ["Bearer ", E.encodeUtf8 (accessToken token)]]
+            $ setRequestPath "/1.1/statuses/user_timeline.json"
+            $ setQueryString [("screen_name", Just name), ("count", Just (toByteString' $ fromMaybe 10 limit))]
+            $ setRequestSecure True
+            $ setRequestPort 443 request'
+    response <- httpJSON request
+    S8.putStrLn $ Yaml.encode (getResponseBody response :: Value)
---
+++ src/Twitter/Config.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Twitter.Config (
+config,
+twitterEncKey
+) where
+
+import           System.Environment
+import           Data.Maybe
+import qualified Data.ByteString.Base64     as B
+import qualified Data.ByteString.Char8      as S8
+import           Data.ByteString.Conversion
+
+
+data Config = Config {
+  twitter :: TwitterConf
+}
+
+data TwitterConf = TwitterConf {
+  consumerKey :: String,
+  consumerSecret :: String
+}
+
+config :: IO Config
+config = do
+  twitterConf <- getTwitterConf
+  return Config { twitter = twitterConf }
+
+twitterEncKey :: Config -> S8.ByteString
+twitterEncKey conf = B.encode byteStr
+    where byteStr = toByteString' concatedKey
+          concatedKey = key ++ ":" ++ secret
+          twitterConf = twitter conf
+          key = consumerKey twitterConf
+          secret = consumerSecret twitterConf
+
+getTwitterConf :: IO TwitterConf
+getTwitterConf = do
+  key <- lookupEnv "TWITTER_CONSUMER_KEY"
+  secret <- lookupEnv "TWITTER_CONSUMER_SECRET"
+  return TwitterConf { consumerKey = fromMaybe "" key, consumerSecret = fromMaybe "" secret }
